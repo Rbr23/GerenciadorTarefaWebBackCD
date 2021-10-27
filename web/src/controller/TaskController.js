41bcb67a5f13242f0a267a03cd69dbd38d25d717
Rbr23 <44401064+Rbr23@users.noreply.github.com>
2021-10-27 10:17:04 -0300

    filtros prontos

--- web/src/controller/TaskController.js
+++ web/src/controller/TaskController.js
@@ -1,5 +1,18 @@
 const TaskModel = require('../model/TaskModel');
 
+const now = new Date();
+
+const {
+       startOfDay, 
+       endOfDay, 
+       startOfWeek, 
+       endOfWeek, 
+       startOfMonth, 
+       endOfMonth, 
+       startOfYear, 
+       endOfYear
+        } = require('date-fns');
+
 //A controller faz o tráfego, recebendo e enviando respostas
 class TaskController{
 
@@ -19,7 +32,7 @@ class TaskController{
 
     }
 
-
+    //Método para atualizar
     async update(req, res){
 
         await TaskModel
@@ -29,6 +42,7 @@ class TaskController{
 
     }
 
+    //Método para leitura de todas as tarefas
     async readAll(req, res){
 
         await TaskModel
@@ -39,6 +53,7 @@ class TaskController{
 
     }
 
+    //Método para leitura de uma tarefa específica
     async readById(req, res){
 
         await TaskModel.findById(req.params.id)
@@ -52,6 +67,7 @@ class TaskController{
 
     }
 
+    //Método para deleção de tarefa
     async delete(req, res){
 
         await TaskModel
@@ -61,7 +77,67 @@ class TaskController{
 
     }
 
-    
+
+    //método responsável por encontrar a tarefa pelo id e atualizar o status de feito ou não feito.
+    async done(req, res){
+
+        await TaskModel
+        .findByIdAndUpdate({'_id':req.params.id},{'done':req.params.done}, {new: true})
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+
+    }
+
+    //método responsável por verificar se a tarefa está atrasada
+    //lt = less... than
+    async late(req, res){
+        await TaskModel
+        .find({'when':{'$lt': now}, 'macadress': {'$in': req.body.macadress} })
+        .sort('when')
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+    }
+
+
+    //filtros => hoje 
+    async today(req, res){
+        await TaskModel
+        .find({'macadress': {'$in': req.body.macadress}, 'when':{'$gte': startOfDay(now), '$lt': endOfDay(now)}})
+        .sort('when')
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+    }
+
+    //filtro => semana
+    async week(req, res){
+        await TaskModel
+        .find({'macadress': {'$in': req.body.macadress}, 'when':{'$gte': startOfWeek(now), '$lt': endOfWeek(now)}})
+        .sort('when')
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+    }
+
+    //filtro => mÊs
+    async month(req, res){
+        await TaskModel
+        .find({'macadress': {'$in': req.body.macadress}, 'when':{'$gte': startOfMonth(now), '$lt': endOfMonth(now)}})
+        .sort('when')
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+    }
+
+    //filtro => ano
+    async year(req, res){
+        await TaskModel
+        .find({'macadress': {'$in': req.body.macadress}, 'when':{'$gte': startOfYear(now), '$lt': endOfYear(now)}})
+        .sort('when')
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+    }
+
+
+
+
 
 }
 
